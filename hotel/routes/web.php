ddb7c92c0bc3325a862cf37505cef2a62a8d4d39
Denis Helienek <denis.helienek@gmail.com>
2017-10-10 20:52:52 +0200

    restauracia fixed

--- hotel/routes/web.php
+++ hotel/routes/web.php
@@ -37,4 +37,4 @@ Route::get('/prihlasenie', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/domov', 'HomeController@index')->name('home');
